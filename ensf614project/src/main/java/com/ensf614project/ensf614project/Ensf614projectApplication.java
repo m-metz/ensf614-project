d7cb2299dcdf22e6103362c18a13ce92b93c3450
KBaldwin2 <kimberley.baldwin@ucalgary.ca>
2022-11-26 11:09:36 -0700

    set up springboot application to start building client on server

---
+++ ensf614project/src/main/java/com/ensf614project/ensf614project/Ensf614projectApplication.java
@@ -0,0 +1,13 @@
+package com.ensf614project.ensf614project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ensf614projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ensf614projectApplication.class, args);
+	}
+
+}
--- ensf614project/src/main/java/movietheatre/App.java
+++
@@ -1,9 +0,0 @@
-package movietheatre;
-
-
-public class App {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
