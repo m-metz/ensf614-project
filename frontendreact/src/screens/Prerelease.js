602f453c3891318ea958f8306cdfcee953f5dbaf
KBaldwin2 <kimberley.baldwin@ucalgary.ca>
2022-12-01 17:24:55 -0700

    Movie fetching working

--- frontendreact/src/screens/Prerelease.js
+++ frontendreact/src/screens/Prerelease.js
@@ -2,22 +2,6 @@ import Movielist from '../components/Movielist';
 import { getFetch } from '../fetch';
 import {useState} from 'react';
 
-//Should be just active movies
-const prereleaseDummy =    [{
-    id: '7',
-    title: "GetSmart",
-    image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/captainmarvel_lob_crd_06.jpg",
-    description: "Element of surprise",
-    Rating: "3"
-},
-{
-    id: '8',
-    title: "Deadpool",
-    image: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/deadpool_lob_crd_02.jpg",
-    description: "Ryan reynolds doing hot boy stuff",
-    Rating: "3"
-}
-]
 
 function Prerelease() {
     const[isLoading, setIsLoading] = useState(true);
