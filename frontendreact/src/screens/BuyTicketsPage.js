f015f07415ca19be496285103b9ec7e23ebd6326
VladT7 <vladt13@gmail.com>
2022-12-02 01:24:58 -0700

    successfully host on aws

--- frontendreact/src/screens/BuyTicketsPage.js
+++ frontendreact/src/screens/BuyTicketsPage.js
@@ -1,11 +1,12 @@
 import { Button } from "@mui/material";
 import Paymentinfo from "../components/Paymentinfo";
+
 export default function BuyTicketsPage() {
   const email = sessionStorage.getItem("currentEmail");
-  const isLoggedIn = email !== null && email !== 'null';
+  const isLoggedIn = email !== null && email !== "null";
+
   return (
     <div>
-
       <h1> Ticket Shopping Cart </h1>
       <h2> You are buying tickets for</h2>
       <p>Movie: {[sessionStorage.getItem("currentMovieId")]}</p>
@@ -13,11 +14,56 @@ export default function BuyTicketsPage() {
       <p>Show time: {[sessionStorage.getItem("currentShowtimeId")]}</p>
       <p>Seats: {[sessionStorage.getItem("selectedSeats")]}</p>
 
-       {isLoggedIn ? (<div>{email}</div>): (<Paymentinfo/>)} 
-      
-      <Button variant = "contained">Buy Tickets</Button>
+      {isLoggedIn ? (
+        <div>We have your payment information on file, {email}</div>
+      ) : (
+        <>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+            id="loginForm"
+          >
+            <label>Enter your email: </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              required
+            ></input>
+            <br></br>
+            <br></br>
+          </form>
+          <Paymentinfo />
+        </>
+      )}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        id="voucherForm"
+      >
+        <label>Enter your voucher id</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          required
+        ></input>
+        <br></br>
+        <br></br>
+      </form>
+      <Button
+        variant="contained"
+        onClick={alert("Great success!")}
+      >
+        Buy Tickets
+      </Button>
     </div>
 
     //show email
+
+    //put in field to put in voucher code
+    //put in grand total
   );
 }
